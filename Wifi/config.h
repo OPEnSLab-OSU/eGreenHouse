5fbd6a1d401ff16b38474e4074b0e30ed6f513cd
Kenneth Kang <gykang00@gmail.com>
2020-06-17 11:03:22 -0700

    Hide my personal information

--- Wifi/config.h
+++ Wifi/config.h
@@ -1,7 +1,7 @@
 "{\
   'general':{'name':'eGreenhouseHubWifi', 'instance':1,'interval':5000},\
   'components':[\
-    {'name':'Wifi','params':['bumblebee','Kkmj1222..']},\
+    {'name':'Wifi','params':['Wifi-name','Wifi-password']},\
     {'name':'GoogleSheets', 'params':['Google', 7001, '/macros/s/AKfycbwf6oReQY0J3T406ikPQYvXPsYsfegRYHDxyXMPGLxepV_OBmoW/exec','1M0TS0sN5z6AMDMXzh-0gRyxqf6c6dWQTrbkpmNTeRXI', false, 'Demo1']},\
     {'name':'LoRa','params':[255,9,23,3,200]}\
   ]\
