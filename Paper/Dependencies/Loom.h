ada59bed80e72f2f0457df98e281aab6cc30f155
MrKangs <gykang00@gmail.com>
2020-06-22 12:42:02 -0700

    Add the paper documentation

---
+++ Paper/Dependencies/Loom.h
@@ -0,0 +1,24 @@
+/*! \mainpage An Internet of Things Rapid Prototyping System for applications in environmental sensing
+
+This is the Doxygen generated documentation of the Loom library code.
+
+\section resources Resources
+
+- [Github](https://github.com/OPEnSLab-OSU/Loom)
+- [Loom Wiki](https://github.com/OPEnSLab-OSU/Loom/wiki)
+- [Project Website](http://www.open-sensing.org/project-loom)
+
+*/
+
+
+
+
+#define LOOM_DEBUG 1
+
+// Preamble will only need to include leaves in the hierarchy
+// abstract (non-leaves) already included by leaves
+
+///////////////////////////////////////////////////////////////////////////////
+// Core Classes
+#include "Module_Factory.h"
+#include "Manager.h"
